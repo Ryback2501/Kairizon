4dd3291a51c4fd8b8eb0df45d4b2f2ba81e79b17
Ryback2501 <deiturbe@gmail.com>
2026-04-28 17:25:36 +0200

    chore(docker): reduce production image size by ~400MB

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const config = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: "https", hostname: "images-na.ssl-images-amazon.com" },
       { protocol: "https", hostname: "m.media-amazon.com" },
